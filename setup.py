5466c062f8b762ca5b3953c1b8a91bf549a54eb2
jmb-python-developer <jmb-python-developer@proton.me>
2024-04-17 18:49:17 +0200

    First thee notebooks uploaded for Linear Regression, Multivariable Linear Regression and Gradient Descent

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project with simple examples created after books or courses lessons. The idea of the project is to help slowly introduce the reader into beginner's machine learning concepts through small (and sometimes even trivial) jupyter notebooks.',
+    author='JuanMBruno',
+    license='',
+)
